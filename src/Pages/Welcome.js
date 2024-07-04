b7237d98355d75607667edb8a7722e46882f6705
howardks <93079698+howardks@users.noreply.github.com>
2024-07-04 11:30:33 -0400

    Test link added for user pages.

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,9 +1,14 @@
-function Header() {
+import { Link } from "react-router-dom";
+
+function Welcome() {
+
     return (
         <div>
             Welcome Page Under Construction
+            <br />
+            <Link to='/web-journal/user/Itsy'>Itsy test link</Link>
         </div>
     )
 }
 
-export default Header;
+export default Welcome;
