5208fabb739473b794f002877cf45283f0fec5d7
howardks <93079698+howardks@users.noreply.github.com>
2024-07-04 08:46:52 -0400

    Css updated.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <div className="Header">
-            <div id="Logo-Div">
+            <span id="Navbar-Brand">
                 <img src={logo} className="Header-Img" alt="Web Icon" />
                 <span className="Header-Text">Web Journal</span>
-            </div>
-            <div id='Navbar'>
+            </span>
+            <span id='Navbar'>
                 <Link className="Nav-Link" to='/web-journal/'><button>Home</button></Link>
                 <Link className="Nav-Link" to='/web-journal/login'><button>Login</button></Link>
                 <Link className="Nav-Link" to='/web-journal/register'><button>Register</button></Link>
-            </div>
+            </span>
         </div>
     )
 }
